96a8c662d056235be27d037dbf480078d926348c
Kevin Parmar <parmarkevinb@gmail.com>
2020-03-25 23:05:08 -0400

    Updated api routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 1024;
+const PORT = process.env.PORT || 2048;
 
 const db = require("./models");
 
@@ -18,6 +18,7 @@ app.use(express.static("public/"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/trainerdb", {
 useNewUrlParser: true });
 
+// html routes
 app.get("/exercise", (req, res) => {
     res.sendfile("./public/exercise.html")
 }) 
@@ -26,6 +27,7 @@ app.get("/stats", (req, res) => {
     res.sendfile("./public/stats.html")
 })
 
+// api routes
 app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
         .then(dbWorkout => {
@@ -34,7 +36,27 @@ app.get("/api/workouts", (req, res) => {
 });
 
 app.post("/api/workouts", (req, res) => {
-    db.Workout.create()
+    db.Workout.create(req.body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+})
+
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, { $push: { workouts: req.body } })
+        .then(dbUser => {
+            res.json(dbUser);
+        })
+});
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+    .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
 })
 
 app.listen(PORT, () => {
